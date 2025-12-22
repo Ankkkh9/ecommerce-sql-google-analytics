13a0f4612877cf9fa5adb95833ae9e452a3910ea
Trần Nguyễn Khang An <khangan.10hlvc@gmail.com>
2025-12-22 22:05:48 +0700

    Update query_03_revenue_time.sql

--- sql/query_03_revenue_time.sql
+++ sql/query_03_revenue_time.sql
@@ -1,20 +1,42 @@
----Q3: Ranking Top 3 TeritoryID with biggest Order quantity of every year. If there's TerritoryID with same quantity in a year, do not skip the rank number
-WITH raw1 AS(SELECT EXTRACT(YEAR FROM sale.ModifiedDate) AS year, TerritoryID, SUM(sale.OrderQty) AS qty_item,
-        FROM `adventureworks2019.Sales.SalesOrderDetail` AS sale
+---Query 03: Revenue by traffic source by week, by month in June 2017
 
 
-        LEFT JOIN `adventureworks2019.Sales.SalesOrderHeader` AS header
-            ON sale.SalesorderID =header.SalesorderID
+--- Create Month and add Time &Time_type column
+WITH Raw_Month AS(SELECT *,
+      FORMAT_DATE('%Y%m', PARSE_DATE('%Y%m%d', date)) AS Time1,
+      'Month' AS Time_type
+      FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201706*`),
+--- Create Week
+      Raw_Week AS(SELECT *,
+            FORMAT_DATE('%Y%W', PARSE_DATE('%Y%m%d', date)) AS Week,
+            'Week' AS Time_type
+      FROM `bigquery-public-data.google_analytics_sample.ga_sessions_201706*`),
 
 
-        GROUP BY 1,2),
----Ranking by quantity item
-raw2 AS(SELECT year, TerritoryID, qty_item,
-                DENSE_RANK() OVER(PARTITION BY year ORDER BY qty_item DESC) AS rnk
-        FROM raw1)
+--- Combine to have raw data & unnest revenue
+      raw_data AS(SELECT Time_type,
+                        Time1,
+                        trafficSource.source AS source,
+                        productRevenue AS revenue  
+                  FROM Raw_Month,
+                  UNNEST (hits) hits,
+                  UNNEST (hits.product) product
+                  WHERE product.productRevenue is not null
+                  UNION ALL
+                  SELECT Time_type,
+                        Week,
+                        trafficSource.source AS source,
+                        productRevenue AS revenue  
+                  FROM Raw_Week,
+                  UNNEST (hits) hits,
+                  UNNEST (hits.product) product
+                  WHERE product.productRevenue is not null)
 
 
-SELECT *
-FROM raw2
-WHERE rnk <=3
-ORDER BY year DESC;
+SELECT Time_type,
+      Time1,
+      source,
+      ROUND(SUM(revenue)/1000000,4) AS revenue
+FROM raw_data
+GROUP BY Time_type, Time1, source
+ORDER BY revenue DESC;
